951889cc120d3cbfc78083380ad5fe26b4901ef3
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-03 15:41:30 +0800

    modify

--- app2.py
+++ app2.py
@@ -6,34 +6,27 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app2 = Dash(requests_pathname_prefix="/app2/",external_stylesheets=external_stylesheets)
 app2.layout = html.Div([
-    html.Div(
-        className="row",
-        children="我的第一個App和圖表顯示,並使用控制項",
-        style={'textAlign':'center','color':'blue','fontSize':30}
-    ),
-    html.Div(className='row',children=[
-        dcc.RadioItems(
-            options=['pop', 'lifeExp', 'gdpPercap'],
-            value='lifeExp',
-            id='my-radio-buttons-final')
+    html.Div(className='row', children='My First App with Data, Graph, and Controls',
+             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+
+    html.Div(className='row', children=[
+        dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
+                       value='lifeExp',
+                       inline=True,
+                       id='my-radio-buttons-final')
     ]),
-    html.Div(className='row',children=[
-        html.Div(
-            className='six columns',children=[
-                dash_table.DataTable(
-                data=df.to_dict('records'),
-                page_size=10,
-                style_table={'overflowX': 'auto'})
-            ]
-        ),
-        html.Div(
-            className='six columns',children=[
-            dcc.Graph(figure={},id='histo-chart-final')
+
+    html.Div(className='row', children=[
+        html.Div(className='six columns', children=[
+            dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+        ]),
+        html.Div(className='six columns', children=[
+            dcc.Graph(figure={}, id='histo-chart-final')
         ])
-    ]),
-    
+    ])
 ])
 
+# Add controls to build the interaction
 @callback(
     Output(component_id='histo-chart-final', component_property='figure'),
     Input(component_id='my-radio-buttons-final', component_property='value')
